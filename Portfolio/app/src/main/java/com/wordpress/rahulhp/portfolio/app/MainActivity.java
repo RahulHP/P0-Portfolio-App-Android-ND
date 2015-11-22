bc6bef5ca26694ba9356e67b6da02611f16a6cac
RahulHP <rahul.hphatak@gmail.com>
2015-11-23 00:11:27 +0530

    Initial

--- Portfolio/app/src/main/java/com/wordpress/rahulhp/portfolio/app/MainActivity.java
+++ Portfolio/app/src/main/java/com/wordpress/rahulhp/portfolio/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wordpress.rahulhp.portfolio.app;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,17 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
 
 
     }
